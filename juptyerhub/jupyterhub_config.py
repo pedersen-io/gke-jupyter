1a1397257dd2adfef55fe86107285e10c8687975
Derek Pedersen <derekpedersen.com@gmail.com>
2018-12-04 16:18:22 -0800

    Have a bootable deployment of jupyterhub that uses github oauth. Trying to use kubespawner to start a notebook server.

--- juptyerhub/jupyterhub_config.py
+++
@@ -1,8 +0,0 @@
-# use github authenticator
-from oauthenticator.github import GitHubOAuthenticator
-c.JupyterHub.authenticator_class = GitHubOAuthenticator
-
-# specify users and admin
-c.Authenticator.whitelist = {'derekpedersen'}
-c.Authenticator.admin_users = {'derekpedersen'}
-
---
+++ jupyterhub/jupyterhub-derek_config.py
@@ -0,0 +1,49 @@
+# jupyterhub_config.py file
+c = get_config()
+
+import os
+pjoin = os.path.join
+
+runtime_dir = os.path.join('/srv/jupyterhub')
+ssl_dir = pjoin(runtime_dir, 'ssl')
+if not os.path.exists(ssl_dir):
+    os.makedirs(ssl_dir)
+
+# Allows multiple single-server per user
+c.JupyterHub.allow_named_servers = True
+
+# https on :443
+# c.JupyterHub.port = 443
+# c.JupyterHub.ssl_key = pjoin(ssl_dir, 'ssl.key')
+# c.JupyterHub.ssl_cert = pjoin(ssl_dir, 'ssl.cert')
+
+# put the JupyterHub cookie secret and state db
+# in /var/run/jupyterhub
+c.JupyterHub.cookie_secret_file = pjoin(runtime_dir, 'cookie_secret')
+c.JupyterHub.db_url = pjoin(runtime_dir, 'jupyterhub.sqlite')
+# or `--db=/path/to/jupyterhub.sqlite` on the command-line
+
+# use GitHub OAuthenticator for local users
+c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
+c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+
+# create system users that don't exist yet
+c.LocalAuthenticator.create_system_users = True
+
+# specify users and admin
+c.Authenticator.whitelist = {'derekpedersen'}
+c.Authenticator.admin_users = {'derekpedersen'}
+
+# uses the default spawner
+# To use a different spawner, uncomment `spawner_class` and set to desired
+# spawner (e.g. SudoSpawner). Follow instructions for desired spawner
+# configuration.
+# c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
+c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
+
+# start single-user notebook servers in ~/assignments,
+# with ~/assignments/Welcome.ipynb as the default landing page
+# this config could also be put in
+# /etc/jupyter/jupyter_notebook_config.py
+c.Spawner.notebook_dir = '~/assignments'
+c.Spawner.args = ['--NotebookApp.default_url=/notebooks/Welcome.ipynb']
